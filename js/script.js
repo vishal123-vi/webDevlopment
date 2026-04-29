22c1c35bb29633fbe98cad56e34d1f66b6f97abb
vishal123-vi <vishaltiwary7878@gmail.com>
2026-04-29 19:29:46 +0530

    javascript

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// for(var i=1;i<=10;i++)
+// {
+//     console.log(i*2);
+// }
+let a=[24,58,68,56,23,35,26,278,345,7]
+// for(var i=1;i<=a.length;i++)
+// {
+//     console.log(a[i]);
+// }
+
+// while loop
+
+// let i=0;
+// while(i<a.length)
+// {
+//     console.log(a[i]);
+//     i++;
+// }
+// do while loop
+let i=0;
+do
+{
+    console.log(a[i])
+    i++;
+}
+while(i<=a.length)
