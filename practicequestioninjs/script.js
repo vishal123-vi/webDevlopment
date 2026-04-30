5b5b4281db573342e4c4a187b4b61b1964347ffc
vishal123-vi <vishaltiwary7878@gmail.com>
2026-04-30 14:10:38 +0530

    practices

---
+++ practicequestioninjs/script.js
@@ -0,0 +1,81 @@
+// 1.   Print numbers from  1  to  5   using a for loop.  
+console.log("question no.1")
+for(var i=1;i<=5;i++)
+{
+    console.log(i);
+}
+// 2.   Print numbers from  1  to  10   using a for loop.  
+
+console.log("question no.2")
+for(var i=1;i<=10;i++)
+{
+    console.log(i);
+}
+// 3.   Print numbers from  10  to 1 (reverse counting ) .   
+console.log("question no.3")
+for(var i=10;i>=1;i--)
+{
+    console.log(i)
+}
+// 4.   Print  even numbers from 2 to 10 .    
+console.log("question no.4")
+console.log("even number")
+for(var i=1;i<=10;i++)
+{
+    if(i%2==0)
+    {
+        console.log(i)
+    }
+}
+
+// 5.   Print  odd numbers from 1 to 9 .   
+
+console.log("question no.5")
+console.log("odd number")
+for(var i=1;i<=10;i++)
+{
+    if(i%2!==0)
+    {
+        console.log(i)
+    }
+}
+
+// 6.   Print the  table of 15   up to 10 steps.  
+console.log("question no.6")
+for(var i=1;i<=10;i++)
+{
+    console.log(15*i)
+}
+
+// 7.   Print numbers from  1  to  20   with increment of 2.
+console.log("question no.7")
+for(var i=1;i<=20;i+=2)
+{
+    console.log(i)
+}
+// 8.   Print numbers from  5  to  50   with increment of 5.
+console.log("question no.8")
+for(var i=5;i<=50;i+=5)
+{
+    console.log(i)
+}
+// 9.   Create a loop and print the  square of numbers  
+// from 1 to 10 .   
+console.log("question no.9")
+for(var i=1;i<=10;i++)
+{
+    console.log(i*i)
+}
+// 10. Print numbers from 1 to 10 that are divisible  
+// by 3.   
+
+console.log("question no.10")
+for(var i=1;i<=10;i++)
+{
+    if(i%3==0)
+    {
+        console.log(i)
+    }
+}
+
+
