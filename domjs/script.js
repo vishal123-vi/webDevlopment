e78e2c88a1263c12469788db2f1bb1f09ab1d709
vishal123-vi <vishaltiwary7878@gmail.com>
2026-05-05 16:21:55 +0530

    add: create initial HTML, CSS, and JavaScript files for form submission and data display

---
+++ domjs/script.js
@@ -0,0 +1,29 @@
+function Submit() {
+  const fullName = document.getElementById("fullName").value;
+  const city = document.getElementById("city").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  document.getElementById("myData").innerText = fullName;
+  document.getElementById("myData2").innerText = city;
+  document.getElementById("myData3").innerText = email;
+  document.getElementById("myData4").innerText = password;
+
+  document.getElementById("dataCard").classList.remove("divHide");
+  document.getElementById("dataCard").classList.add("divShow");
+}
+
+function clearForm() {
+  document.getElementById("fullName").value = "";
+  document.getElementById("city").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+
+  document.getElementById("myData").innerText = "";
+  document.getElementById("myData2").innerText = "";
+  document.getElementById("myData3").innerText = "";
+  document.getElementById("myData4").innerText = "";
+
+  document.getElementById("dataCard").classList.remove("divShow");
+  document.getElementById("dataCard").classList.add("divHide");
+}
