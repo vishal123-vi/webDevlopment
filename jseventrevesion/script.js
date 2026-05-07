a7cc03abf456a6bb354bd0b69c0bb6a389a35f27
vishal123-vi <vishaltiwary7878@gmail.com>
2026-05-08 00:18:32 +0530

    fix: update color input event listeners to use 'input' for real-time updates and add paragraph color functionality

--- jseventrevesion/script.js
+++ jseventrevesion/script.js
@@ -1,12 +1,21 @@
-document.getElementById("bgColor").addEventListener("change", ChangeColor);
+document.getElementById("bgColor").addEventListener("input", ChangeColor);
 function ChangeColor() {
   const color = document.getElementById("bgColor").value;
   document.getElementById("changeColor").style.backgroundColor = color;
 }
 
-//
-document.getElementById("headingColor").addEventListener("change", ChangeColor1);
-function changecolor1() {
+document.getElementById("headingColor").addEventListener("input", ChangeColor1);
+function ChangeColor1() {
   const color = document.getElementById("headingColor").value;
   document.getElementById("heading").style.color = color;
+}
+
+document.getElementById("paraColor").addEventListener("input", ChangeColor2);
+function ChangeColor2() {
+  const color = document.getElementById("paraColor").value;
+  document.getElementById("para").style.color = color;
+}
+
+function reset(){
+    window.location.reload();
 }
