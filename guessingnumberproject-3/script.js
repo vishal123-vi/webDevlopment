f4a4da11056b0306645e0437e0b60aa28bcab549
vishal123-vi <vishaltiwary7878@gmail.com>
2026-05-10 02:11:46 +0530

    add: create initial HTML and JavaScript files for Guess The Number game

---
+++ guessingnumberproject-3/script.js
@@ -0,0 +1,19 @@
+const number = Math.floor(Math.random() * 10) + 1;
+
+function alert() {
+  const guess = parseInt(document.getElementById("guess").value, 10);
+  
+  if (guess > number) {
+    document.getElementById("msg").innerText =
+      "OOPS! SORRY!!! TRY A SMALLER NUMBER.";
+  } else if (guess < number) {
+    document.getElementById("msg").innerText =
+      "OOPS! SORRY!!! TRY A LARGER NUMBER.";
+  }
+//    else if (guess === number) {
+//     document.getElementById("msg").innerText = "Congratulations! You guessed it right!";
+//   }
+   else {
+    document.getElementById("msg").innerText = "Please enter a valid number.";
+  }
+}
