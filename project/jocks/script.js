58f5f0df79826f5b976c0dfec98ba410c79338a2
vishal123-vi <vishaltiwary7878@gmail.com>
2026-05-23 22:27:13 +0530

    fix: format code for consistency in GetnewJoke function

--- project/jocks/script.js
+++ project/jocks/script.js
@@ -1,8 +1,8 @@
 async function GetnewJoke() {
-    const API_URL = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark?contains=1";
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    document.getElementById("setup").innerText=data.joke||data.setup;
-    document.getElementById("delivery").innerText = data.delivery||"";
-    
+  const API_URL =
+    "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark?contains=1";
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  document.getElementById("setup").innerText = data.joke || data.setup;
+  document.getElementById("delivery").innerText = data.delivery || "";
 }
